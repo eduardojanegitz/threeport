53e6a6994590c03569959e633aa6c15c31616dd8
Eduardo Alves de Lima Filho <877@vitafor.local>
2023-04-28 01:39:37 -0300

    about section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
@@ -24,34 +24,53 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
-  )
-}
+  );
+};
 
 const ComputersCanvas = () => {
-  return (
-    <Canvas
-      frameloop="demand"
-      shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Computers />
-      </Suspense>
+  const [isMobile, setIsMobile] = useState(false);
 
-      <Preload all />
-    </Canvas>
-  )
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    };
+  }, []);
+
+return (
+  <Canvas
+    frameloop="demand"
+    shadows
+    dpr={[1, 2]}
+    camera={{ position: [20, 3, 5], fov: 25 }}
+    gl={{ preserveDrawingBuffer: true }}
+  >
+    <Suspense fallback={<CanvasLoader />}>
+      <OrbitControls
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+      />
+      <Computers isMobile={isMobile} />
+    </Suspense>
+
+    <Preload all />
+  </Canvas>
+)
 }
 
 export default ComputersCanvas;
--- src/constants/index.js
+++ src/constants/index.js
@@ -42,19 +42,19 @@ import {
   
   const services = [
     {
-      title: "Web Developer",
+      title: "Desenvolvedor Web",
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Desenvolvedor React",
       icon: mobile,
     },
     {
-      title: "Backend Developer",
+      title: "Desenvolvedor Backend",
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Criador de Conteúdo",
       icon: creator,
     },
   ];
